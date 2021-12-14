6bffcd218ec8d374587ed9369702b3401e99125e
Sarah dePalo <sarahdepalo99@gmail.com>
2021-12-14 11:18:48 -0500

    created backend folder, sql, schema

---
+++ backend/sql/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO prompts
+    (question)
+VALUES
+    ('What are you thankful for today?');
+
+INSERT INTO journal
+    (user_id, prompt_id, entry_date, response)
+VALUES
+    ('97c00769-b8e7-47b5-9a1a-8da087d9ada5'. 1, '1999-01-08 04:05:06', 'I am thankful for my having an apartment with my boyfriend. After living with parents for the past year and a half, it feels so nice to wake up in my own little space each morning. Cooking food in my own kitchen or drinking tea on my couch bring me joy lately. As well as the beautiful nature scene outside my window. ')
---
+++ backend/sql/shcema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users(
+    id UUID DEFAULT uuid_generate_v4(),
+    nickname VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email_Verified BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE prompts (
+    id serial PRIMARY KEY,
+    question text NOT NULL
+);
+
+CREATE TABLE journal (
+    id serial PRIMARY KEY,
+    user_id UUID references users(id),
+    prompt_id integer references prompts(id),
+    entry_date timestamp without time zone NOT NULL, 
+    response text
+);
