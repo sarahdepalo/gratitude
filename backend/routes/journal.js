1bf221f1b013b721a25c4c68fd63711ad8dd84f8
Sarah dePalo <sarahdepalo99@gmail.com>
2021-12-15 12:34:59 -0500

    journal route responds with custom error message if no entries are found for that month

--- backend/routes/journal.js
+++ backend/routes/journal.js
@@ -7,7 +7,14 @@ const JournalModel = require("../models/Journal");
 router.get("/", async (req, res) => {
     const { user_id, month } = req.body;
     const entries = await JournalModel.getMonthlyEntries(user_id, month);
-    res.json(entries).status(200);
+    if(entries.length > 0 ) {
+        res.json(entries).status(200);
+    } else {
+        res.json({
+            "error": "No entries found."
+        }).status(500)
+    }
+   
 });
 
 
