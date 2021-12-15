d5c0af45c4a65ff75b3998b388d49bb9896afd90
Sarah dePalo <sarahdepalo99@gmail.com>
2021-12-15 16:11:13 -0500

    finished most navbar and dashboard styles including responsiveness

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,12 +1,25 @@
-import  LogoutButton  from "../userAuth/Logout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faBook, faDoorOpen } from "@fortawesome/free-solid-svg-icons";
 import "./navbar.scss";
 
 const Navbar = () => {
   return (
     <>
       <nav>
-        <p>Navbar</p>
-        <LogoutButton/>
+        <ul className="icons"> 
+          <li>
+            <FontAwesomeIcon icon={faHome} size="lg" className="home" />
+            <p>Prompt</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faBook} size="lg" className="book" />
+            <p>Journal</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faDoorOpen} size="lg" className="door" />
+            <p>Logout</p>
+          </li>
+        </ul>
       </nav>
     </>
   );
